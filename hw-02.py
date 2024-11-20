b4b418b623e85b84bc7f77b8be617cc50e3f5cf1
Arkady Gude <gude@list.ru>
2024-11-20 10:34:02 +0300

    Completion of the task

--- hw-02.py
+++ hw-02.py
@@ -8,30 +8,33 @@ class Student:
         self.grades = {}
 
     def rate_lecturer(self, lecturer, course, grade):
-        if isinstance(lecturer,
-                      Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
-            if course in lecturer.grades:
-                lecturer.grades[course] += [grade]
-            else:
-                lecturer.grades[course] = [grade]
-        else:
-            return 'Ошибка'
 
-    def average_grades_student(self, student, grades):
-        sum_grades = 0
-        for k, v in grades.items():
-            sum_grades += sum(v)
-        self.average_grade_student = sum_grades / len(student.grades.values())
-        return self.average_grade_student
+        if not isinstance(lecturer, Lecturer):
+            raise ValueError("Оценка может быть поставлена только лектору")
+        elif course not in self.courses_in_progress:
+            raise ValueError("Курс не числится в списке изучаемых")
+        elif course not in lecturer.courses_attached:
+            raise ValueError("Курс не закреплен за лектором")
+        elif course in lecturer.grades:
+            lecturer.grades[course] += [grade]
+        else:
+            lecturer.grades[course] = [grade]
+
+    def averagegrade(self):
+        return calculateaverage(self.grades)
 
     def __lt__(self, student):
-        return (self.average_grades_student == average_grades_student)
+        if not isinstance(other, Student):
+            raise TypeError("Сравнение возможно только между студентами")
+        return (self.averagegrade() < averagegrade())
 
     def __gt__(self, student):
-        return (self.average_grades_student == average_grades_student)
+        if not isinstance(other, Student):
+            raise TypeError("Сравнение возможно только между студентами")
+        return (self.averagegrade() > averagegrade())
 
     def __str__(self):
-        return f'Студент\nИмя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.average_grade_student}\nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}\n'
+        return f'Студент\nИмя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.averagegrade()}\nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}\n'
 
 
 class Mentor:
@@ -43,37 +46,49 @@ class Mentor:
 
 class Lecturer(Mentor):
 
-    def average_grades_lecturer(self, lecturer, grades):
-        sum_grades = 0
-        for k, v in lecturer.grades.items():
-            sum_grades += sum(v)
-        self.average_grade_lecturer = sum_grades / len(lecturer.grades.values())
-        # print(average_grade_lecturer)
-        return self.average_grade_lecturer
+    def averagegrade(self):
+        return calculateaverage(self.grades)
 
-    def __lt__(self, lecturer):
-        return (self.average_grades_lecturer == lecturer.average_grades_lecturer)
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            raise TypeError("Сравнение возможно только между лекторами")
+        return (self.averagegrade() < other.averagegrade())
 
-    def __gt__(self, lecturer):
-        return (self.average_grades_lecturer == lecturer.average_grades_lecturer)
+    def __gt__(self, other):
+        if not isinstance(other, Lecturer):
+            raise TypeError("Сравнение возможно только между лекторами")
+        return (self.averagegrade() > other.averagegrade())
 
     def __str__(self):
-        return f'Лектор\nИмя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции:{self.average_grade_lecturer}\n'
+        return f'Лектор\nИмя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции:{self.averagegrade()}\n'
 
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
+
+        if not isinstance(student, Student):
+            raise ValueError("Оценка может быть поставлена только студенту")
+        elif course not in student.courses_in_progress:
+            raise ValueError("Курс не числится в списке изучаемых")
+        elif course not in self.courses_attached:
+            raise ValueError("Курс не закреплен за лектором")
+        elif course in student.grades:
+            student.grades[course] += [grade]
         else:
-            return 'Ошибка'
+            student.grades[course] = [grade]
 
     def __str__(self):
         return f'Ревьюер\nИмя: {self.name}\nФамилия: {self.surname}\n'
 
+
+# Универсальная функция расчета средних оценок для студентов и лекторов
+def calculateaverage(grades):
+    """Универсальная функция подсчета средней оценки."""
+    totalgrades = [grade for gradeslist in grades.values() for grade in gradeslist]
+    return sum(totalgrades) / len(totalgrades) if totalgrades else 0
+
+
+# Создание экземпляров классов
 
 student1 = Student('Иван', 'Петров', 'мужской')
 student1.courses_in_progress += ['Python']
@@ -101,12 +116,17 @@ lecturer2.courses_attached += ['GIT']
 student1.rate_lecturer(lecturer1, 'Python', 9)
 student2.rate_lecturer(lecturer2, 'GIT', 8)
 
-student1_average_grades = student1.average_grades_student(student1, student1.grades)
-student2_average_grades = student2.average_grades_student(student2, student2.grades)
 
-lecturer1_average_grades = lecturer1.average_grades_lecturer(lecturer1, lecturer1.grades)
-lecturer2_average_grades = lecturer2.average_grades_lecturer(lecturer2, lecturer2.grades)
+# Расчет средних оценок
 
+student1_average_grades = student1.averagegrade()
+student2_average_grades = student2.averagegrade()
+
+lecturer1_average_grades = lecturer1.averagegrade()
+lecturer2_average_grades = lecturer2.averagegrade()
+
+
+# Вывод списков студентов, лекторов и ревьюеров
 
 print(student1)
 print(student2)
@@ -115,6 +135,7 @@ print(reviewer2)
 print(lecturer1)
 print(lecturer2)
 
+# Вывод сравнительных оценок для лекторов
 
 if lecturer1_average_grades > lecturer2_average_grades:
     print(f'У лектора {lecturer1.name} оценка {lecturer1_average_grades} выше чем у лектора {lecturer2.name} с оценкой {lecturer2_average_grades}')
@@ -124,6 +145,7 @@ elif lecturer1_average_grades < lecturer2_average_grades:
 else:
     print(f'У лектора {lecturer1.name} и у лектора {lecturer2.name} оценки одинаковые {lecturer1_average_grades} и {lecturer2_average_grades}')
 
+# Вывод сравнительных оценок для студентов
 
 if student1_average_grades > student2_average_grades:
     print(f'У студента {student1.name} оценка {student1_average_grades} выше чем у студента {student2.name} с оценкой {student2_average_grades}')
